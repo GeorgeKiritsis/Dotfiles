1b96f09fb093fcb8b88f62c957072dea645fb3b7
GeorgeKiritsis <georgekiritsis5@gmail.com>
2024-05-24 09:43:40 +0300

    Added StatusLine

--- nvim/lua/kir/plugins/toggleterm.lua
+++ nvim/lua/kir/plugins/toggleterm.lua
@@ -24,7 +24,7 @@ return {
           title_pos = 'center' -- 'left', 'center', 'right'
         },
         winbar = {
-          enabled = false,
+          enabled = true,
           name_formatter = function(term) -- term: Terminal
             return term.name
           end
